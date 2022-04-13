9ba4840e7ce0fd834841d449ce986a2508b1b253
santhoshth <thoomatti.santhosh.w@gmail.com>
2022-04-13 19:27:18 +0530

    firebase setup and Sign up, Sign in

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+// configuration object copied from firebase project settings
 const firebaseConfig = {
-
     apiKey: "AIzaSyAVQl9HL4AdYNA-orCCNPHrdXC42YP8gbs",
-
     authDomain: "amazn-clone-santhosh.firebaseapp.com",
-
     projectId: "amazn-clone-santhosh",
-
     storageBucket: "amazn-clone-santhosh.appspot.com",
-
     messagingSenderId: "14929816413",
-
     appId: "1:14929816413:web:1b0321201b6eed6c07d6b4"
-
 };
+
+// Initialize App with the firebase configuration
+const app = initializeApp(firebaseConfig);
+
+// Initialize database
+// firestore is real time database in firebase
+export const db = getFirestore(app);
+
+// for Authentication
+export const auth = getAuth(app);
